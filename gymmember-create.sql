fd49d491fc0294936ccdda733aef725675497c9b
Lance <Metal_Death_Music@yahoo.com>
2021-11-08 17:23:27 -0500

    gym member update

--- gymmember-create.sql
+++ gymmember-create.sql
@@ -22,5 +22,11 @@ create table membership (
     insert into membership VALUES (2, 65, "24/7 access", "included spa");
     insert into membership VALUES (3, 45, "24/7 access", "");
     
-    select * from membership;
+    insert into members VALUES (1, "Bob Hope", "1298 Hope Ave", "Hopeville", "NE", "39485", "948-892-2845", "BHope@Gmail.com", 1);
+    insert into members VALUES (2, "Sue Rangle", "850 Sprat Drive", "Pinkle", "WI", "12845", "468-432-4182", "Suegle@yahoo.com", 2);
+    insert into members VALUES (3, "Prince Jangles", "128 Terras Court", "Snupton", "CA", "92834", "923-281-6817", "PrinceJ@Gmail.com", 1);
+    insert into members VALUES (4, "Tomas Hawson", "6218 Center Blvd", "Eclear", "PA", "19388", "294-024-9284", "Tomawson@Gmail.com", 3);
+    
+    select m.cost, m.access, m.inclusions, me.name, me.phone, me.email from membership m
+    join members me on me.membershipID = m.membershipID;
     
