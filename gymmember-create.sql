b048e44548e029096e7b0a90133e93715fa040c3
Lance <Metal_Death_Music@yahoo.com>
2022-03-16 16:00:07 -0400

    create new PRS2

--- gymmember-create.sql
+++ gymmember-create.sql
@@ -15,18 +15,23 @@ create table membership (
         phone varchar(12) not null,
         email varchar(30),
         membershipID integer not null,
+        accessCode integer not null unique,
         constraint foreign key (membershipID) references membership(membershipID)
         );
+        
+        drop table members;
         
 	insert into membership VALUES (1, 85, "24/7 access", "included spa, private coaching sessions incl.");
     insert into membership VALUES (2, 65, "24/7 access", "included spa");
     insert into membership VALUES (3, 45, "24/7 access", "");
     
-    insert into members VALUES (1, "Bob Hope", "1298 Hope Ave", "Hopeville", "NE", "39485", "948-892-2845", "BHope@Gmail.com", 1);
-    insert into members VALUES (2, "Sue Rangle", "850 Sprat Drive", "Pinkle", "WI", "12845", "468-432-4182", "Suegle@yahoo.com", 2);
-    insert into members VALUES (3, "Prince Jangles", "128 Terras Court", "Snupton", "CA", "92834", "923-281-6817", "PrinceJ@Gmail.com", 1);
-    insert into members VALUES (4, "Tomas Hawson", "6218 Center Blvd", "Eclear", "PA", "19388", "294-024-9284", "Tomawson@Gmail.com", 3);
+    insert into members VALUES (1, "Bob Hope", "1298 Hope Ave", "Hopeville", "NE", "39485", "948-892-2845", "BHope@Gmail.com", 1, 1238);
+    insert into members VALUES (2, "Sue Rangle", "850 Sprat Drive", "Pinkle", "WI", "12845", "468-432-4182", "Suegle@yahoo.com", 2, 9384);
+    insert into members VALUES (3, "Prince Jangles", "128 Terras Court", "Snupton", "CA", "92834", "923-281-6817", "PrinceJ@Gmail.com", 1, 9562);
+    insert into members VALUES (4, "Tomas Hawson", "6218 Center Blvd", "Eclear", "PA", "19388", "294-024-9284", "Tomawson@Gmail.com", 3, 4138);
     
     select m.cost, m.access, m.inclusions, me.name, me.phone, me.email from membership m
     join members me on me.membershipID = m.membershipID;
+    
+    select * from members;
     
